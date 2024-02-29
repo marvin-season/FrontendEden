5b60413ec3f5382e3f44fea232de18bd67979558
marvin <sea-son@foxmail.com>
2024-02-29 15:06:33 +0800

    多包依赖倒入

---
+++ packages/shared/vite.config.ts
@@ -0,0 +1,19 @@
+// vite.config.js
+import {resolve} from 'path'
+import {defineConfig} from "vite";
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'shared',
+            fileName: "shared",
+            formats: ["es", "cjs", "iife"]
+        },
+    },
+    plugins: [dts({
+        outputDir: 'dist/types',
+        insertTypesEntry: true
+    })]
+})
