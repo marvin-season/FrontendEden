d7c5c8eafb62baf09caa0c261189ae6f9ae047bf
marvin <sea-son@foxmail.com>
2024-03-01 11:12:55 +0800

    pdf

--- packages/react-front-research/src/pages/PDFPreview/index.tsx
+++ packages/react-front-research/src/pages/PDFPreview/index.tsx
@@ -1,31 +1,16 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {PdfViewer} from "@/components/pdf/PdfViewer.tsx";
-import pdf from '/demo2.pdf'
 
 const keywords = {
     demo6: '了众多超级极客和高瞻远瞩的商业领袖'
 }
 
 function Index() {
-    const [value, setValue] = useState(keywords.demo6)
-
-    const [url, setUrl] = useState('/demo2.pdf')
-    const [file, setFile] = useState<Blob>()
+    const [url, setUrl] = useState('/demo.pdf')
 
     return <div>
-        <input type={"file"} onChange={(e) => {
-            let file = e.currentTarget.files?.[0];
-            if(file){
-                let blob = new Blob(['hello'], {type: file.type});
-                console.log(blob, file.type)
-                setFile(blob)
-            }
-        }}/>
-        <button onClick={() => {
-            setUrl('/demo.pdf')
-        }}>toggle</button>
         <div style={{height: `500px`, overflow: "auto",}}>
-            <PdfViewer url={file}
+            <PdfViewer url={url}
                        width={800} searchText={value}/>;
         </div>
 
