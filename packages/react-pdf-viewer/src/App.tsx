c6c28f842ef323f3016457db2b1b02bbf4493efc
marvin <sea-son@foxmail.com>
2024-03-02 16:28:22 +0800

    step1: render

--- packages/react-pdf-viewer/src/App.tsx
+++ packages/react-pdf-viewer/src/App.tsx
@@ -1,9 +1,32 @@
 import PDFViewer from "@/components/PDFViewer.tsx";
+import {ChangeEventHandler, useEffect, useState} from "react";
+import * as pdfjsLib from "pdfjs-dist";
 
 function App() {
+    const [file, setFile] = useState<Blob>();
+    const handleFileChange: ChangeEventHandler<HTMLInputElement> = async (e) => {
+        let file: File | undefined = e.currentTarget.files?.[0];
+        if (!file) {
+            return
+        }
+        setFile(file)
+    }
+
+    useEffect(() => {
+        (async () => {
+            if (file) {
+                const pdf = await pdfjsLib.getDocument(await file.arrayBuffer()).promise;
+                let page = await pdf.getPage(1);
+                let textContent = await page.getTextContent();
+                console.log('textContent', textContent)
+
+            }
+        })()
+    }, [file]);
     return (
         <>
-            <PDFViewer/>
+            <input type={'file'} onChange={handleFileChange}/>
+            <PDFViewer file={file}/>
         </>
     )
 }
--- packages/react-pdf-viewer/src/components/PDFViewer.tsx
+++ packages/react-pdf-viewer/src/components/PDFViewer.tsx
@@ -1,5 +1,22 @@
-const PDFViewer = () => {
-    return <>pdf
+import React from "react";
+import './index.css'
+import 'react-pdf/dist/Page/TextLayer.css';
+import {Document, Page, pdfjs} from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    "pdfjs-dist/build/pdf.worker.min.js",
+    import.meta.url
+).toString();
+const PDFViewer: React.FC<{
+    file: string | Blob | ArrayBuffer | undefined
+}> = ({
+          file
+      }) => {
+    return <>
+
+        <Document file={file}>
+            <Page renderAnnotationLayer={false} width={800} pageNumber={1}></Page>
+        </Document>
     </>
 }
 
