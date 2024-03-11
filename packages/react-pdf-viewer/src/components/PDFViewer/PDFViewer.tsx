08fe471461a955e0b2ba30a7807e41419d0c216e
marvin <sea-son@foxmail.com>
2024-03-11 14:42:43 +0800

    动态加载页码

--- packages/react-pdf-viewer/src/components/PDFViewer/PDFViewer.tsx
+++ packages/react-pdf-viewer/src/components/PDFViewer/PDFViewer.tsx
@@ -46,7 +46,7 @@ export const PDFViewer: React.FC<PDFProps> = ({
                                                   width
                                               }) => {
 
-    const [_, setNumPages, getNumPages] = useGetState(0);
+    const [_, setNumPages, getNumPages] = useGetState(0); // 页数
     const [hlSet, setHlSet] = useState<HighlightSet>(new Set([]));
     const [renderTextLayer, setRenderTextLayer] = useState(false)
     const pdfDocumentProxyRef = useRef<PDFDocumentProxy>();
@@ -97,17 +97,39 @@ export const PDFViewer: React.FC<PDFProps> = ({
         </Page>;
     };
 
-    const handleAddFirst = lodash.debounce(() => {
-        console.log('顶部', getPageRenderRange());
-        if (getPageRenderRange().at(0) >= 1) {
-            setPageRenderRange([getPageRenderRange().at(0) - 1, ...getPageRenderRange()])
+    const handleAddFirst = lodash.debounce((number = 3) => {
+        console.log(`<- 加载n${number}页`, getPageRenderRange());
+        const firstPageIndex = getPageRenderRange().at(0);
+        if (firstPageIndex >= 1) {
+            const newPageIndex = []
+            // = [firstPageIndex - 1, firstPageIndex - 2, firstPageIndex - 3].filter(index => index >= 0);
+            // [lastPageIndex + 1, lastPageIndex + 2, lastPageIndex + 3].filter(index => index <= getNumPages())
+            for (let i = 1; i <= number; i++) {
+                if (firstPageIndex - i < 0) {
+                    break
+                }
+
+                newPageIndex.unshift(firstPageIndex - i)
+            }
+            setPageRenderRange([...newPageIndex, ...getPageRenderRange()])
         }
     }, 1000)
 
-    const handleAddLast = useCallback(lodash.debounce(() => {
-        console.log('底部', pageRenderRange);
-        if (getPageRenderRange().at(-1) <= getNumPages()) {
-            setPageRenderRange([...getPageRenderRange(), getPageRenderRange().at(-1) + 1])
+    const handleAddLast = useCallback(lodash.debounce((number = 3) => {
+        console.log(`-> 加载n${number}页`, getPageRenderRange(), getNumPages());
+
+        const lastPageIndex = getPageRenderRange().at(-1);
+        if (lastPageIndex <= getNumPages() - 2) {
+            const newPageIndex = []
+            // [lastPageIndex + 1, lastPageIndex + 2, lastPageIndex + 3].filter(index => index <= getNumPages())
+            for (let i = 1; i <= number; i++) {
+                if (lastPageIndex + i > getNumPages() - 1) {
+                    break
+                }
+
+                newPageIndex.push(lastPageIndex + i)
+            }
+            setPageRenderRange([...getPageRenderRange(), ...newPageIndex])
         }
     }, 1000), [pageRenderRange])
 
@@ -120,7 +142,6 @@ export const PDFViewer: React.FC<PDFProps> = ({
         }
         documentContainerDom.addEventListener('scroll', (e) => {
             const {height: documentHeight} = documentDom.getBoundingClientRect();
-            console.log('documentContainerDom.scrollTop', documentContainerDom.scrollTop, documentHeight);
             // 提前两页加载
             if (documentContainerDom.scrollTop <= 800 * 2) {
                 handleAddFirst()
