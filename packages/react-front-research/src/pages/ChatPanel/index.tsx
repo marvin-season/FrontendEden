47fedd14707cb4a391266adf99b548ac7093a3dd
marvin <sea-son@foxmail.com>
2024-03-12 17:10:35 +0800

    Chat组件封装布局树

--- packages/react-front-research/src/pages/ChatPanel/index.tsx
+++ packages/react-front-research/src/pages/ChatPanel/index.tsx
@@ -1,4 +1,6 @@
-import {Chat, ChatItem} from "@/components/chat/Chat.tsx";
+import {Chat} from "@/components/chat/Chat.tsx";
+import {ChatItem} from "@/components/chat/types.ts";
+import {treeLayoutRender} from "@/pages/ChatPanel/render.tsx";
 
 const chatList: ChatItem[] = [
     {
@@ -10,22 +12,29 @@ const chatList: ChatItem[] = [
         id: '2',
         content: 'hello',
         role: 'answer',
+        parentId: '1'
     },
     {
         id: '3',
+        content: '我是您的AI助手',
+        role: 'answer',
+        parentId: '1'
+    },
+    {
+        id: '4',
         content: 'hi',
         role: 'question',
     },
     {
-        id: '4',
+        id: '5',
         content: 'hello',
         role: 'answer',
-        parentId: '3'
+        parentId: '4'
     }
 ]
 export default function () {
 
     return <>
-        <Chat chatList={chatList}/>
+        <Chat chatList={chatList} renderChatItemLayout={treeLayoutRender}/>
     </>
 }
---
+++ packages/react-front-research/src/pages/ChatPanel/render.tsx
@@ -0,0 +1,6 @@
+import {ChatProps} from "@/components/chat/types.ts";
+
+export const treeLayoutRender: ChatProps['renderChatItemLayout'] = () => {
+    return <>
+    </>
+}
