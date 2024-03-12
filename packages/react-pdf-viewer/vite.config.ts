cea21943a29ab4f8da0d861f42e59a45bc1a5187
marvin <sea-son@foxmail.com>
2024-03-12 14:55:56 +0800

    change es lib

--- packages/react-pdf-viewer/vite.config.ts
+++ packages/react-pdf-viewer/vite.config.ts
@@ -5,6 +5,14 @@ import {resolve} from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+      target: 'esnext'
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            target: 'esnext',
+        },
+    },
     server: {
         port: 10004,
 
