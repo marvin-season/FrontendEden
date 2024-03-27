7aebf79c1bb7388172947a02121e858e585468ef
marvin <sea-son@foxmail.com>
2024-03-27 17:29:31 +0800

    requestAnimationFrame

--- packages/react-pdf-viewer/src/components/Task/index.tsx
+++ packages/react-pdf-viewer/src/components/Task/index.tsx
@@ -1,24 +1,22 @@
 import {useEffect, useRef, useState} from "react";
 
 export const Task = () => {
-    const [value, setValue] = useState<string>('')
+    const [value, setValue] = useState<string>('');
     const containerRef = useRef<HTMLDivElement>(null);
     useEffect(() => {
-        const timer = setInterval(() => {
-            setValue(prevState => prevState + '哈')
-        }, 10)
-        return () => {
-            timer && clearInterval(timer)
-        }
+        const callback = () => {
+            setValue(prevState => prevState + '哈');
+            requestAnimationFrame(callback)
+        };
+        requestAnimationFrame(callback)
+        // const timer = setInterval(() => {
+        //     setValue(prevState => prevState + '哈')
+        // }, 10)
+        // return () => {
+        //     timer && clearInterval(timer)
+        // }
     }, []);
 
-    useEffect(() => {
-        const current = containerRef.current;
-        if (current && current.scrollHeight > current.clientHeight) {
-            current.scrollTop = current.scrollHeight;
-
-        }
-    }, [value]);
 
     return <div ref={containerRef} style={{height: '100px', overflow: "auto"}}>
         <h5>value:</h5>
