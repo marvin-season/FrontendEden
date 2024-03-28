7216674ac6e03f552a4859e7a68db68550232b39
marvin <sea-son@foxmail.com>
2024-03-28 14:09:11 +0800

    groupRenderLayout

--- packages/react-front-research/src/pages/ChatPanel/index.tsx
+++ packages/react-front-research/src/pages/ChatPanel/index.tsx
@@ -2,12 +2,13 @@ import {FC} from "react";
 import {useChat} from "@/pages/ChatPanel/useChat.ts";
 import {connect} from "@/components/chat/hoc.tsx";
 import {Chat} from "@/components/chat";
+import {groupRenderLayout} from "@/pages/ChatPanel/groupRender.tsx";
 
 const ChatPanel: FC = () => {
     const chatProps = useChat();
     const ConnectedChat = connect(Chat, chatProps);
 
-    return <ConnectedChat/>;
+    return <ConnectedChat renderChatItemLayout={groupRenderLayout}/>;
 }
 
 export default ChatPanel
