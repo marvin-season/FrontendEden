cec99b6aed07c698ad0b509109d1f0dec994fca1
marvin <sea-son@foxmail.com>
2024-03-28 11:46:49 +0800

    reload func

--- packages/react-front-research/src/pages/ChatPanel/index.tsx
+++ packages/react-front-research/src/pages/ChatPanel/index.tsx
@@ -4,40 +4,7 @@ import {useState} from "react";
 import {nanoid} from "nanoid";
 import {groupRenderLayout} from "@/pages/ChatPanel/groupRender.tsx";
 
-const nanoid1 = nanoid()
-
-const list: ChatItem[] = [
-    {
-        groupId: nanoid(),
-        id: '1',
-        content: '你吃饭了没',
-        role: 'question',
-    },
-    {
-        groupId: nanoid1,
-        id: '2',
-        content: 'hello，我吃过了',
-        role: 'answer',
-    },
-    {
-        groupId: nanoid1,
-        id: '3',
-        content: '青椒炒肉',
-        role: 'answer',
-    },
-    {
-        groupId: nanoid(),
-        id: '5',
-        content: '今天星期几？',
-        role: 'question',
-    },
-    {
-        groupId: nanoid(),
-        id: '7',
-        content: '今天星期三？',
-        role: 'answer',
-    },
-]
+const list: ChatItem[] = []
 export default function () {
     const [chatList, setChatList] = useState<TreeChatItem[]>(list);
     const [baseAttach, setBaseAttach] = useState<BaseAttach>({})
@@ -61,7 +28,11 @@ export default function () {
                     images
                 })
             }}
-            onReload={console.log}
+            onReload={(chatItem) => {
+                const clone = structuredClone(chatItem);
+                clone.content = clone.content.concat('reloaded')
+                setChatList(chatList.concat(clone))
+            }}
             onSend={(value) => {
                 setChatList(chatList.concat({
                     id: Date.now().toString(),
