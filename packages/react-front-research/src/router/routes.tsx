206b291d173238b1290f8a172c7b294690bf95d4
marvin <sea-son@foxmail.com>
2024-03-29 14:00:31 +0800

    删除pdf预览

--- packages/react-front-research/src/router/routes.tsx
+++ packages/react-front-research/src/router/routes.tsx
@@ -1,6 +1,5 @@
 import {RouteObject} from "react-router-dom";
 import Layout from "@/layout";
-import PDFPreview from "@/pages/PDFPreview";
 import Index from "@/pages/Index";
 import FilePage from "@/pages/FilePage";
 import React from "react";
@@ -18,11 +17,6 @@ export const routes: RouteObject[] = [
                 path: 'index',
                 index: true,
                 element: <Index/>
-            },
-            {
-                path: 'pdf-preview',
-                index: true,
-                element: <PDFPreview/>
             },
             {
                 path: 'blob-file-buffer',
