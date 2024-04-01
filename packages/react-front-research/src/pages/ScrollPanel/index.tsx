abb20c0d8a786cc87b4f9cc4f609b19c301e9528
marvin <sea-son@foxmail.com>
2024-04-01 10:42:42 +0800

    textLayer

--- packages/react-front-research/src/pages/ScrollPanel/index.tsx
+++ packages/react-front-research/src/pages/ScrollPanel/index.tsx
@@ -1,47 +1,50 @@
 import './index.css'
+import {useState} from "react";
 
 const ScrollPanel = () => {
+    const [count, setCount] = useState(0)
+
+    const textLayer = <div className={'textLayer'}>
+        <span className="item" id="item1">1</span>
+        <span className="item" id="item2">2</span>
+        <span className="item" id="item3">3</span>
+        <span className="item" id="item4">4</span>
+        <span className="item" id="item5">5</span>
+        <span className="item" id="item6">6</span>
+    </div>
+
+
     return <div className={'flex'}>
         <div className={'container'}>
-            <div>
-                <div className={'page1'}>
-                    <span className="item" id="item1">1</span>
-                    <span className="item" id="item2">2</span>
-                    <span className="item" id="item3">3</span>
-                    <span className="item" id="item4">4</span>
-                    <span className="item" id="item5">5</span>
-                    <span className="item" id="item6">6</span>
-                    <div className={'page'}></div>
-                </div>
+            <div className='page page1'>
+                <div className={'canvas'}></div>
+                {textLayer}
             </div>
-            <div>
-
-                <div className={'page2'}>
-                    <span className="item" id="item1">1</span>
-                    <span className="item" id="item2">2</span>
-                    <span className="item" id="item3">3</span>
-                    <span className="item" id="item4">4</span>
-                    <span className="item" id="item5">5</span>
-                    <span className="item" id="item6">6</span>
-                    <div className={'page'}></div>
-                </div>
+            <div className='page page2'>
+                <div className={'canvas'}></div>
+                {textLayer}
             </div>
 
         </div>
         <div>
             <button onClick={() => {
-                const target = document.querySelector('.page1>#item2');
-                target.scrollIntoView({
+                setCount(count + 1)
+                const target = document.querySelectorAll('.page1 .textLayer .item');
+                target[1].classList.add('hl')
+                target[1]?.scrollIntoView({
                     behavior: 'smooth',
-                    block: 'center'
+                    block: 'start'
                 })
             }}>skip2
             </button>
             <button onClick={() => {
-                const target = document.querySelector('.page2>#item5');
-                target.scrollIntoView({
+                setCount(count + 1)
+
+                const target = document.querySelectorAll('.page2 .textLayer .item');
+                target[4].classList.add('hl')
+                target[4]?.scrollIntoView({
                     behavior: 'smooth',
-                    block: 'center'
+                    block: 'start'
                 })
             }}>skip5
             </button>
