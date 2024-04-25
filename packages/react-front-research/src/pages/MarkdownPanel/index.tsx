5816591567d9e7460624943daaf16ffed13463a9
marvin <sea-son@foxmail.com>
2024-04-25 15:53:00 +0800

    md

--- packages/react-front-research/src/pages/MarkdownPanel/index.tsx
+++ packages/react-front-research/src/pages/MarkdownPanel/index.tsx
@@ -1,24 +1,32 @@
-import ReactMarkdown from '@uiw/react-markdown-preview'
-import {useEffect, useState} from "react";
-import {readLines} from '@/utils/file.ts';
-const demo = "你好这是一段文字\n\n```c\n#include <stdio.h>```"
+import Markdown from "@/components/markdown";
+import {Button, Flex} from "antd";
+import {useState} from "react";
+
 const MarkdownPanel = () => {
-    const [source, setSource] = useState('')
-    useEffect(() => {
-        readLines('/kspx', (data) => {
-            if (data) {
-                setSource(prevState => prevState.concat(data))
-            }
-        })
-    }, []);
+    const [markdown, setMarkdown] = useState("")
 
+    return <Flex>
+        <Button onClick={() => {
+            const blob = new Blob([markdown], {type: 'text/markdown'});
+            const link = document.createElement('a');
 
-    return <div style={{width: '600px', backgroundColor: 'lightcyan', padding: '20px'}}>
-        <ReactMarkdown style={{
-            background: 'lightcyan',
-            fontSize: '12px'
-        }} source={source}></ReactMarkdown>
-    </div>
+            // Set the href attribute of the link to the Blob object
+            link.href = window.URL.createObjectURL(blob);
+
+            // Set the download attribute of the link to specify the filename
+            link.download = 'new_file.md';
+
+            // Append the link to the body
+            document.body.appendChild(link);
+
+            // Trigger a click event on the link to start the download
+            link.click();
+
+            // Remove the link from the body
+            document.body.removeChild(link);
+        }}>导出</Button>
+        <Markdown onChange={setMarkdown} markdown={markdown} />
+    </Flex>
 }
 
 export default MarkdownPanel
