a9266fc09755e14a53fde7d0ce6c6260cc6b0500
marvin <sea-son@foxmail.com>
2024-04-26 10:41:18 +0800

    try catch

--- packages/react-front-research/src/hook/useDownload.ts
+++ packages/react-front-research/src/hook/useDownload.ts
@@ -4,6 +4,7 @@ import {exportFile} from "@/utils/file.ts";
 type ProgressCallback = (receivedLength: number, totalLength: number) => void;
 type SuccessCallback = (blob: Blob) => void;
 type CancelCallback = () => void;
+type ErrorCallback = (e: unknown) => void;
 
 interface DownloadOptions {
     contentType?: string;
@@ -16,39 +17,46 @@ export const useDownload = (
     onSuccess: SuccessCallback = () => {
     },
     onCancel: CancelCallback = () => {
+    },
+    onError: ErrorCallback = () => {
     }
 ) => {
     const controller = useRef<AbortController>();
 
     const download = async (url: string, {contentType, isExport}: DownloadOptions) => {
         controller.current = new AbortController();
-        const res = await fetch(url, {
-            signal: controller.current.signal
-        });
-        // 文件总长度
-        const contentLength = res.headers.get('content-length');
+        try {
+            const res = await fetch(url, {
+                signal: controller.current.signal
+            });
+            // 文件总长度
+            const contentLength = res.headers.get('content-length');
 
-        if (!res.body || !contentLength) {
-            return
-        }
-        const reader = res.body.getReader();
-        let receivedLength = 0;
-
-        const chunks: Uint8Array[] = [];
-        while (true) {
-            const {value, done} = await reader.read();
-            if (done) {
-                break
+            if (!res.body || !contentLength) {
+                return
             }
-            chunks.push(value);
-            receivedLength += value.length;
-            onProgress(receivedLength, +contentLength)
+            const reader = res.body.getReader();
+            let receivedLength = 0;
+
+            const chunks: Uint8Array[] = [];
+            while (true) {
+                const {value, done} = await reader.read();
+                if (done) {
+                    break
+                }
+                chunks.push(value);
+                receivedLength += value.length;
+                onProgress(receivedLength, +contentLength)
+            }
+
+            const blob = new Blob(chunks, {type: res.headers.get("content-type") || contentType});
+
+            isExport && exportFile(blob, url);
+            onSuccess(blob);
+        } catch (e) {
+            onError(e);
+        } finally {
         }
-
-        const blob = new Blob(chunks, {type: res.headers.get("content-type") || contentType});
-
-        isExport && exportFile(blob, url);
-        onSuccess(blob);
 
     }
 
