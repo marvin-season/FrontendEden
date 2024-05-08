949f3c8c653bee288eceac239cd4c3fae1a8e7b8
marvin <sea-son@foxmail.com>
2024-05-08 10:28:01 +0800

    fix: anno test stream

--- packages/react-front-research/src/main.tsx
+++ packages/react-front-research/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import {Providers} from "./providers";
-import "@/test/stream.ts"
+// import "@/test/stream.ts"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
