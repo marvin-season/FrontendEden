830bf2ae4f27c8371ceb40ec28fd0bc543897674
marvin <sea-son@foxmail.com>
2024-05-14 14:53:36 +0800

    perf: 删除没必要的文件

--- packages/react-front-research/src/pages/ChatPanel/index.tsx
+++ packages/react-front-research/src/pages/ChatPanel/index.tsx
@@ -1,18 +1,16 @@
 import {FC, useEffect} from "react";
 import {useChat} from "@/pages/ChatPanel/useChat.ts";
-import {connect} from "@/components/chat/hoc.tsx";
 import {Chat} from "@/components/chat";
 import {groupRenderLayout} from "@/pages/ChatPanel/groupRender.tsx";
 
 const ChatPanel: FC = () => {
     const chatProps = useChat();
-    const ConnectedChat = connect(Chat, chatProps);
 
     useEffect(() => {
         fetch('/v2/chat').then()
     }, []);
 
-    return <ConnectedChat renderChatItemLayout={groupRenderLayout}/>;
+    return <Chat {...chatProps} renderChatItemLayout={groupRenderLayout}/>;
 }
 
 export default ChatPanel
