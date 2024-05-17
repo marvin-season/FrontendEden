32ed4c705beb1ac30244b0c3333af36b9bc1f6ca
marvin <sea-son@foxmail.com>
2024-05-17 10:41:13 +0800

    fix: chat with default render

--- packages/react-ui/lib/components/Chat/Chat.tsx
+++ packages/react-ui/lib/components/Chat/Chat.tsx
@@ -2,19 +2,24 @@ import React, {FC} from "react";
 import {ChatProps} from "@/types/chat.tsx";
 import {ChatContext} from "./context/ChatContext.tsx";
 import {ChatList, UserInput} from "./default/DefaultLayout.tsx";
+import {Flex} from "antd";
 import {
     defaultAnswerPanelRender,
     defaultLinerLayoutRender,
     defaultQuestionPanelRender
-} from "./default/DefaultRender.tsx";
-import {Flex} from "antd";
+} from "@/components/Chat/default/DefaultRender.tsx";
 
-export const Chat: FC<ChatProps> = (props) => {
+export const Chat: FC<ChatProps> = ({
+                                        renderQuestionPanel = defaultQuestionPanelRender,
+                                        renderChatItemLayout = defaultLinerLayoutRender,
+                                        renderAnswerPanel = defaultAnswerPanelRender,
+                                        ...restProps
+                                    }) => {
     return <ChatContext.Provider value={{
-        ...props,
-        renderQuestionPanel: defaultQuestionPanelRender,
-        renderChatItemLayout: defaultLinerLayoutRender,
-        renderAnswerPanel: defaultAnswerPanelRender,
+        renderQuestionPanel,
+        renderChatItemLayout,
+        renderAnswerPanel,
+        ...restProps,
     }}>
         <Flex vertical={true} gap={6} className={'h-full p-6'}>
             <Flex vertical={true} className={'h-2/3 overflow-y-auto border-slate-200 p-4 border rounded-lg'}>
