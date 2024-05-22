ed685102d84cfa7e5baba68d552e8c8d909d66a5
marvin <sea-son@foxmail.com>
2024-05-22 17:19:33 +0800

    chore: Page3 AST

---
+++ packages/react-i18n-ast/src/pages/Page3.tsx
@@ -0,0 +1,22 @@
+import {name} from '../utils'
+
+export const Page2 = () => {
+    const address = "武汉";
+    const renderFn = () => <>
+        renderFn jsx
+    </>
+
+    const ele = <>ele jsx</>
+    return <>
+        <p>{name}</p>
+        <p>{address}</p>
+        <p>
+            {`我的名字：${name}`}
+        </p>
+        <div>
+            <p>{'东湖'}</p>
+        </div>
+        <p>{renderFn()}</p>
+        <p>{ele}</p>
+    </>
+}
