8d3fdecaaf1cd7aa68763de61534aa2012f63c4a
marvin <sea-son@foxmail.com>
2024-05-28 17:00:27 +0800

    feat: open-ai

--- packages/express-based/src/index.ts
+++ packages/express-based/src/index.ts
@@ -1,8 +1,14 @@
 // src/index.js
-import express, { Express, Request, Response } from "express";
+import express, {Express, Request, Response} from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
+
+import OpenAI from 'openai'
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +16,15 @@ const port = process.env.PORT || 3000;
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
 });
+
+app.get("/completion", async (req: Request, res: Response) => {
+    const completion = await openai.chat.completions.create({
+        messages: [{role: "system", content: "You are a helpful assistant."}],
+        model: "gpt-3.5-turbo",
+    });
+    console.log(completion.choices[0]);
+    res.send(JSON.stringify(completion, null, 2));
+})
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
