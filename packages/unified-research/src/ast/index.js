c27077e6ab100de8ec305cfa8cf5b84069f3d74d
marvin <sea-son@foxmail.com>
2024-05-31 14:00:32 +0800

    feat: 配置ts

---
+++ packages/unified-research/src/ast/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var node = {
+    data: undefined, position: undefined, type: ""
+};
+var literal = {
+    data: undefined, position: undefined, type: "", value: undefined
+};
+var parent = {
+    children: [],
+    type: ""
+};
---
+++ packages/unified-research/src/ast/index.ts
@@ -0,0 +1,15 @@
+import type {Literal, Node, Parent} from 'unist'
+
+const node: Node = {
+    data: undefined, position: undefined, type: ""
+
+}
+const literal: Literal = {
+    data: undefined, position: undefined, type: "", value: undefined
+
+}
+
+const parent: Parent = {
+    children: [],
+    type: ""
+}
