6bbe9753c1dd35118bb8e085401a4560658e5baa
marvin <sea-son@foxmail.com>
2024-05-31 09:52:55 +0800

    feat: unified 初始化

---
+++ packages/unified-research/index.js
@@ -0,0 +1,18 @@
+import rehypeDocument from 'rehype-document'
+import rehypeFormat from 'rehype-format'
+import rehypeStringify from 'rehype-stringify'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import {unified} from 'unified'
+import {reporter} from 'vfile-reporter'
+
+const file = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeDocument, {title: '👋🌍'})
+    .use(rehypeFormat)
+    .use(rehypeStringify)
+    .process('# Hello world!')
+
+console.error(reporter(file))
+console.log(String(file))
