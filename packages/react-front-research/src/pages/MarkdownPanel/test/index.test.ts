4e58ad6c67608ebf4b7faf7b2a39141945cf996a
marvin <sea-son@foxmail.com>
2024-06-06 15:05:32 +0800

    test: 字符串匹配

--- packages/react-front-research/src/pages/MarkdownPanel/test/index.test.ts
+++ packages/react-front-research/src/pages/MarkdownPanel/test/index.test.ts
@@ -1,7 +1,14 @@
 // sum.test.js
 import {expect, test} from 'vitest'
-import {getIntersection} from "@/pages/MarkdownPanel/hook.ts";
+import {getHighlightInfo, getIntersection} from "@/pages/MarkdownPanel/utils";
 
 test('交集', () => {
     expect(getIntersection([1, 3], [2, 4])).toEqual([2, 3])
+})
+
+
+test('字符串匹配', async () => {
+    const result = await getHighlightInfo('abcd', 'bc');
+    console.log("🚀  ", result)
+    expect(result).toEqual([1, 3])
 })
