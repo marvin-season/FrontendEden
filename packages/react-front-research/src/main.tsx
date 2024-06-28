b2377af705242aa9c8189826d3364852149b368b
marvin <sea-son@foxmail.com>
2024-06-28 09:39:06 +0800

    fix: layout

--- packages/react-front-research/src/main.tsx
+++ packages/react-front-research/src/main.tsx
@@ -1,28 +1,31 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import ReactDOM from 'react-dom/client'
 import {Providers} from "@/providers";
 import App from "@/App.tsx";
 
 async function enableMocking() {
-    if (process.env.NODE_ENV !== 'development') {
-        return
-    }
+  if (process.env.NODE_ENV !== 'development') {
+    return
+  }
 
-    const {worker} = await import('./mocks/browser')
+  const {worker} = await import('./mocks/browser')
 
-    // `worker.start()` returns a Promise that resolves
-    // once the Service Worker is up and ready to intercept requests.
-    return worker.start()
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start()
 }
 
 enableMocking().then(() => {
-    ReactDOM.createRoot(document.getElementById('root')!).render(
-        <React.StrictMode>
-            <Providers>
-                <App/>
-            </Providers>
-        </React.StrictMode>,
-    )
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Providers>
+          <App/>
+        </Providers>
+      </Suspense>
+
+    </React.StrictMode>,
+  )
 })
 
 
--- packages/react-front-research/vite.config.ts
+++ packages/react-front-research/vite.config.ts
@@ -5,54 +5,60 @@ import Pages from 'vite-plugin-pages'
 import {resolve} from 'path';
 
 export default defineConfig({
-    plugins: [react(), Pages({
-        dirs: [{
-            dir: 'src/pages',
-            filePattern: '**/*-page/index.tsx',
-            baseRoute: '/'
-        }],
-        exclude: ['**/components/*'],
-        importMode: 'sync'
-    })],
-    optimizeDeps: {
-        esbuildOptions: {
-            target: 'esnext',
-        }
-    },
-    server: {
-        port: 10003,
-        proxy: {
-            '/api/': {
-                target: 'http://10.0.44.34:5000', // 开发环境
-                ws: true,
-                changeOrigin: true,
-            },
-            '/api_/': {
-                target: 'https://mock.mengxuegu.com/mock/6618fb2ad985433db203dcb9/',
-                changeOrigin: true,
-                rewrite: path => path.replace('/api/', '')
-            },
-            '/v2/': {
-                target: 'http://127.0.0.1:5000',
-                changeOrigin: true,
-                rewrite: path => path.replace('/v2/', '')
-            },
-            '/w3c/': {
-                target: 'https://www.w3.org',
-                changeOrigin: true,
-                rewrite: path => path.replace('/w3c/', '')
-            },
-            '/api2/': {
-                target: 'http://127.0.0.1:8080',
-                changeOrigin: true,
-                rewrite: path => path.replace('/api2/', '')
-            },
+  plugins: [react(), Pages({
+    dirs: [{
+      dir: 'src/pages',
+      filePattern: '**/*-page/index.tsx',
+      baseRoute: '/'
+    }],
+    exclude: ['**/components/*'],
+    importMode: (filepath, options) => {
+      for (const page of options.dirs) {
+        if (page.baseRoute === '' && filepath.startsWith(`/${page.dir}/index`))
+          return 'sync'
+      }
+      return 'async'
+    }
+  })],
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext',
+    }
+  },
+  server: {
+    port: 10003,
+    proxy: {
+      '/api/': {
+        target: 'http://10.0.44.34:5000', // 开发环境
+        ws: true,
+        changeOrigin: true,
+      },
+      '/api_/': {
+        target: 'https://mock.mengxuegu.com/mock/6618fb2ad985433db203dcb9/',
+        changeOrigin: true,
+        rewrite: path => path.replace('/api/', '')
+      },
+      '/v2/': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        rewrite: path => path.replace('/v2/', '')
+      },
+      '/w3c/': {
+        target: 'https://www.w3.org',
+        changeOrigin: true,
+        rewrite: path => path.replace('/w3c/', '')
+      },
+      '/api2/': {
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true,
+        rewrite: path => path.replace('/api2/', '')
+      },
 
-        }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
     },
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src'),
-        },
-    },
+  },
 })
