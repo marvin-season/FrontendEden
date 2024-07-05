20648da352e7b584e24312ec8ddd8822639311ac
marvin <sea-son@foxmail.com>
2024-07-05 11:21:22 +0800

    feat: App

--- packages/react-perform/src/App.tsx
+++ packages/react-perform/src/App.tsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import React from 'react'
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import DisplayCount from './DisplayCount';
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('Component did mount or update');
+    return () => {
+      console.log('Component will unmount');
+    };
+  }, [count]);
+
+  const increment = () => {
+    console.log('Increment button clicked');
+    setCount(prevCount => {
+      console.log('Updating state:', prevCount + 1);
+      return prevCount + 1;
+    });
+  };
 
   return (
-    <>
-      <div>
-as
-      </div>
-
-    </>
-  )
+    <div>
+      <DisplayCount count={count} />
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
