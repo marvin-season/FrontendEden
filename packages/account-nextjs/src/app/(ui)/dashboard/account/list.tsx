5316d5a666c823da5a1f8af7ea22ec3589801b92
marvin <sea-son@foxmail.com>
2024-07-08 14:58:31 +0800

    fix: api invoke demo

--- packages/account-nextjs/src/app/(ui)/dashboard/account/list.tsx
+++ packages/account-nextjs/src/app/(ui)/dashboard/account/list.tsx
@@ -1,4 +1,10 @@
+"use client";
+import { useEffect } from "react"
+
 export const List = () => {
+    useEffect(() => {
+        fetch('/api/account/list')
+    }, [])
     return <>
         list
     </>
