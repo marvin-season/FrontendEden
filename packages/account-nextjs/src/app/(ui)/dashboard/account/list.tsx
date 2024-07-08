b5ddd1ea2c26795c96b7ed2c306de340b3a7f1ec
marvin <sea-son@foxmail.com>
2024-07-08 18:28:52 +0800

    fix: antd Modal

--- packages/account-nextjs/src/app/(ui)/dashboard/account/list.tsx
+++ packages/account-nextjs/src/app/(ui)/dashboard/account/list.tsx
@@ -2,7 +2,7 @@
 
 import {getAccountList} from "@/app/_service/account";
 import React, {useEffect, useState} from "react";
-import {Button, Flex, Table} from 'antd';
+import {Button, Card, Descriptions, Flex, Modal, Table} from 'antd';
 
 const columns = [
   {
@@ -17,21 +17,39 @@ const columns = [
 
 export const List = () => {
   const [data, setData] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentData, setCurrentData] = useState<any>(null)
   useEffect(() => {
     getAccountList().then(setData)
   }, [])
 
-  // 一个表格，类似antd的样式
-  return <Table dataSource={data}>
-    <Table.Column title="ID" dataIndex="id" key="id"/>
-    <Table.Column title="名称" dataIndex="name" key="name"/>
-    <Table.Column title="操作" render={(_, record, index) => {
-      return <Flex gap={4}>
-        <Button size={'small'} onClick={() => {
-        }}>{'查看'}</Button>
-        <Button size={'small'} onClick={() => {
-        }}>{'隐藏'}</Button>
-      </Flex>
-    }}></Table.Column>
-  </Table>
+  return <>
+    <Modal title={'modal'} open={isModalOpen} onOk={() => {
+      setIsModalOpen(false)
+    }} onCancel={() => {
+      setIsModalOpen(false)
+    }}>
+      <Card>
+        {currentData && <Descriptions>
+          <Descriptions.Item label={'ID'}>{currentData.id}</Descriptions.Item>
+          <Descriptions.Item label={'名称'}>{currentData.name}</Descriptions.Item>
+        </Descriptions>}
+      </Card>
+
+    </Modal>
+    <Table dataSource={data}>
+      <Table.Column title="ID" dataIndex="id" key="id"/>
+      <Table.Column title="名称" dataIndex="name" key="name"/>
+      <Table.Column title="操作" render={(_, record, index) => {
+        return <Flex gap={4}>
+          <Button size={'small'} onClick={() => {
+            setIsModalOpen(true);
+            setCurrentData(record)
+          }}>{'查看'}</Button>
+          <Button size={'small'} onClick={() => {
+          }}>{'隐藏'}</Button>
+        </Flex>
+      }}></Table.Column>
+    </Table>
+  </>
 }
