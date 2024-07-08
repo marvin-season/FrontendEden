92a322e7a26acd67a41eff37821376404cba87b1
marvin <sea-son@foxmail.com>
2024-07-08 18:08:11 +0800

    fix: antd Table

--- packages/account-nextjs/src/app/(ui)/dashboard/account/list.tsx
+++ packages/account-nextjs/src/app/(ui)/dashboard/account/list.tsx
@@ -1,18 +1,37 @@
 "use client";
 
-import { getAccountList } from "@/app/_service/account";
-import { useEffect, useState } from "react";
+import {getAccountList} from "@/app/_service/account";
+import React, {useEffect, useState} from "react";
+import {Button, Flex, Table} from 'antd';
+
+const columns = [
+  {
+    title: 'ID',
+    dataIndex: 'id',
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+]
 
 export const List = () => {
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        getAccountList().then(setData)
-    }, [])
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getAccountList().then(setData)
+  }, [])
 
-    // 一个表格，类似antd的样式
-    return <div className="border-2 rounded-lg p-4">
-        {data.map((item: any) => {
-            return <div className="text-base" key={item.id}>{item.name}</div>
-        })}
-    </div>
+  // 一个表格，类似antd的样式
+  return <Table dataSource={data}>
+    <Table.Column title="ID" dataIndex="id" key="id"/>
+    <Table.Column title="名称" dataIndex="name" key="name"/>
+    <Table.Column title="操作" render={(_, record, index) => {
+      return <Flex gap={4}>
+        <Button size={'small'} onClick={() => {
+        }}>{'查看'}</Button>
+        <Button size={'small'} onClick={() => {
+        }}>{'隐藏'}</Button>
+      </Flex>
+    }}></Table.Column>
+  </Table>
 }
--- packages/account-nextjs/src/app/api/account/list/route.ts
+++ packages/account-nextjs/src/app/api/account/list/route.ts
@@ -1,9 +1,9 @@
 export async function GET() {
   const data = [
     { id: 1, name: 'Account 2024-7-1' },
-    { id: 1, name: 'Account 2024-7-1' },
-    { id: 1, name: 'Account 2024-7-1' },
-    { id: 1, name: 'Account 2024-7-1' },
+    { id: 2, name: 'Account 2024-7-2' },
+    { id: 3, name: 'Account 2024-7-3' },
+    { id: 4, name: 'Account 2024-7-4' },
   ]
 
   return Response.json(data);
