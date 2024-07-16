f7613dc8a6fed30d7c84a85c2cda7deb76d362ec
marvin <sea-son@foxmail.com>
2024-07-16 18:06:58 +0800

    feat: tts s6

--- packages/react-research/src/pages/beautiful-tool-page/tts/index.tsx
+++ packages/react-research/src/pages/beautiful-tool-page/tts/index.tsx
@@ -1,12 +1,21 @@
 import {Button} from 'antd';
 import {TTSMonitor} from '@/utils/tts/TTS.ts'
+import {useState} from 'react';
+import {generateRandomTextWithCallback} from '@/utils/ContentGenerator.ts';
 
 const tts = new TTSMonitor();
 const TTS = () => {
+  const [content, setContent] = useState('');
   return <>
-
+    <div>{content}</div>
     <Button onClick={() => {
-      tts.readAloud('你好<p>我是小燕<p>很高兴认识你').then()
+      setContent('')
+      generateRandomTextWithCallback(['六出祁山', '千里走单骑'], item => {
+        setContent(prevState => prevState.concat(item?.content || ''))
+      })
+    }}>start</Button>
+    <Button onClick={() => {
+      tts.readAloud(content).then()
     }}>send</Button>
     <Button onClick={() => {
       tts.getPlayer().pause().then(console.log).catch(console.log)
--- packages/react-research/src/utils/ContentGenerator.ts
+++ packages/react-research/src/utils/ContentGenerator.ts
@@ -2,43 +2,43 @@ import {nanoid} from "nanoid";
 
 // 随机生成 1 到 n 之间的数字
 function generateRandomNumber(n: number) {
-    return Math.floor(Math.random() * n) + 1;
+  return Math.floor(Math.random() * n) + 1;
 }
 
 export function generateRandomTextWithCallback(
-    onTextGenerated: (text?: { id: string; content: string }) => void
+  words = [
+    "\n**测试**",
+    "RAG **测试**",
+    "RAG **测试**",
+  ],
+  onTextGenerated: (text?: { id: string; content: string }) => void
 ) {
-    // const count = generateRandomNumber(10);
-    const count = 7;
-    const id = nanoid();
-    let index = 0;
+  // const count = generateRandomNumber(10);
+  const count = 3;
+  const id = nanoid();
+  let index = 0;
 
-    const t = setInterval(() => {
-        if (index < count) {
-            const content = generateRandomSentence();
-            onTextGenerated({content, id});
-            index++;
-        } else {
-            clearInterval(t);
-            onTextGenerated()
-        }
-    }, 500);
+  const t = setInterval(() => {
+    if (index < count) {
+      const content = generateRandomSentence(2, words);
+      onTextGenerated({content, id});
+      index++;
+    } else {
+      clearInterval(t);
+      onTextGenerated()
+    }
+  }, 1000);
 
-    return t;
+  return t;
 }
 
-export function generateRandomSentence(wordsLength = 2) {
-    const words = [
-        "\n**测试**",
-        "RAG **测试**",
-        "RAG **测试**",
-    ];
+export function generateRandomSentence(wordsLength = 2, words: string[]) {
 
-    // 随机生成句子长度
-    const sentenceLength = generateRandomNumber(wordsLength);
+  // 随机生成句子长度
+  const sentenceLength = generateRandomNumber(wordsLength);
 
-    return Array.from({length: sentenceLength}, () => {
-        const randomIndex = generateRandomNumber(words.length - 1);
-        return words[randomIndex];
-    }).join(", ")
+  return Array.from({length: sentenceLength}, () => {
+    const randomIndex = generateRandomNumber(words.length - 1);
+    return words[randomIndex];
+  }).join(", ")
 }
