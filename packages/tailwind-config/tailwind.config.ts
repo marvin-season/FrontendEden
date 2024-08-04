eeb8848896dd379722276c7e2dfee24ba6189a73
marvin <sea-son@foxmail.com>
2024-08-04 14:16:24 +0800

    tailwind

---
+++ packages/tailwind-config/tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./src/**/*.tsx', 'node_modules/@marvin/react-ai/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
