c49df9723ac7ac92ece2d4ab10b5b3007e99391c
marvin <sea-son@foxmail.com>
2024-08-04 20:33:49 +0800

    fix: 引入tailwinconfig

---
+++ packages/react-ui/postcss.config.js
@@ -0,0 +1,1 @@
+export { default } from "@marvin/tailwind-config/postcss.config.js";
--- packages/react-ui/src/App.tsx
+++ packages/react-ui/src/App.tsx
@@ -21,11 +21,11 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className='underline'>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className='underline'>
         Click on the Vite and React logos to learn more
       </p>
     </>
