2453f54a92adb9b66333187f268041b02e686946
marvin <sea-son@foxmail.com>
2024-08-27 11:47:47 +0800

    fix: tailwind support for *.jsx

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: ['./src/**/*.tsx', 'node_modules/@marvin/react-ai/**/*.tsx'],
+  content: ['./src/**/*.tsx', './src/**/*.jsx', 'node_modules/@marvin/react-ai/**/*.tsx'],
   theme: {
     extend: {},
   },
