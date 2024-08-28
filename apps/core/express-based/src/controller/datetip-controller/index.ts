e1611998d1af32108fd93f14f1cbbc820c58c637
marvin <sea-son@foxmail.com>
2024-08-28 18:30:26 +0800

    feat: create or update

---
+++ apps/core/express-based/src/controller/datetip-controller/index.ts
@@ -0,0 +1,27 @@
+import {Router} from 'express';
+import prisma from "../../utils/prisma";
+
+const DateTipController = Router();
+
+DateTipController.get('/', async (req, res) => {
+  const data = await prisma.dateTip.findMany()
+  return res.json({data})
+})
+
+DateTipController.post('/:name', async (req, res) => {
+  const data = req.body;
+  let result = null;
+  if (data.id) {
+    result = await prisma.dateTip.update({
+      where: {
+        id: data.id
+      }, data: {}
+    })
+  } else {
+    result = await prisma.dateTip.create({data})
+  }
+
+  return res.json({result})
+})
+
+export default DateTipController
