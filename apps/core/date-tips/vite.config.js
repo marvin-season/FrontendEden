8797261b507cddc03a2165b17d1782ec0b14fc68
marvin <sea-son@foxmail.com>
2024-08-28 15:13:18 +0800

    fix: proxy frontend

--- apps/core/date-tips/vite.config.js
+++ apps/core/date-tips/vite.config.js
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: 7777
+        port: 7777,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+            }
+        }
     }
 })
