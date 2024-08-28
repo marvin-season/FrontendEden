67137348c2842ce5a78501e16ac84c454543fe0b
marvin <sea-son@foxmail.com>
2024-08-28 15:19:56 +0800

    fix: middleware logger

---
+++ apps/core/express-based/src/middleware/logger.ts
@@ -0,0 +1,14 @@
+import {Request, Response, NextFunction} from "express";
+
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  const time = new Date();
+  console.log("===============================start=============================");
+  console.log(`[${time.toLocaleString()}] ${req.method} ${req.url}`);
+  console.log(`[${time.toLocaleString()}] body: ${JSON.stringify(req.body)}`);
+  console.log(`[${time.toLocaleString()}] params:  ${JSON.stringify(req.params)}`);
+  console.log("===============================end=============================");
+  next();
+};
+
+export default logger
