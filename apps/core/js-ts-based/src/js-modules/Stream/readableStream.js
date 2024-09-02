64657b22020f23cd4dad9b2708567818bdf8af4c
marvin <sea-son@foxmail.com>
2024-09-02 10:41:34 +0800

    feat: transformedStream base

--- apps/core/js-ts-based/src/js-modules/Stream/readableStream.js
+++ apps/core/js-ts-based/src/js-modules/Stream/readableStream.js
@@ -1,3 +1,19 @@
-new ReadableStream({
-    type: "bytes"
-}, {})
+const readableStream = new ReadableStream({
+    start(controller) {
+        setInterval(() => {
+            controller.enqueue('hi')
+        }, 1000)
+    },
+})
+
+const transformedStream = readableStream.pipeThrough(new TransformStream({
+        transform(chunk, controller) {
+            console.log('chunk => ', chunk)
+            controller.enqueue(chunk + '1')
+        }
+    }
+))
+
+for await (const transformedStreamElement of transformedStream) {
+    console.log('transformedStreamElement', transformedStreamElement)
+}
