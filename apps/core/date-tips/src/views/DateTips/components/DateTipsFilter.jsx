0833b74a7aba838df035d6c9c3a27038e5a0d2da
marvin <sea-son@foxmail.com>
2024-09-02 15:56:17 +0800

    feat: 日历组件

--- apps/core/date-tips/src/views/DateTips/components/DateTipsFilter.jsx
+++ apps/core/date-tips/src/views/DateTips/components/DateTipsFilter.jsx
@@ -1,15 +1,38 @@
 import {useState} from "react";
+import {Day, DayPicker} from "react-day-picker";
+import {isSameDay} from "date-fns";
 
 export default function DateTipsFilter() {
     const [input, setInput] = useState('');
-    const [dateRange, setDateRange] = useState([])
-
+    const [selected, setSelected] = useState([])
+    const handleDayClick = (day, modifiers) => {
+        console.log({day, modifiers})
+        const newValue = [...selected];
+        if (modifiers.selected) {
+            const index = selected.findIndex((d) => isSameDay(day, d));
+            newValue.splice(index, 1);
+        } else {
+            newValue.push(day);
+        }
+        setSelected(newValue);
+    };
     return <>
         <input className={'border p-2'} type="text" value={input} onChange={
             e => setInput(e.target.value)
         }/>
-        <input className={'border p-2'} type="date" value={dateRange[0]} onChange={
-            e => setDateRange([e.target.value, dateRange[1]])
-        }/>
+
+        <DayPicker
+            onDayClick={handleDayClick}
+            modifiers={{selected}}
+            mode="multiple"
+            components={{
+                // Day: Day
+                Day(props) {
+                    return <>
+                        <Day {...props} className={`${props.modifiers.selected ? 'bg-blue-300' : ''} w-[40px] hover:bg-blue-300`}/>
+                    </>
+                }
+            }}
+        />
     </>
 }
