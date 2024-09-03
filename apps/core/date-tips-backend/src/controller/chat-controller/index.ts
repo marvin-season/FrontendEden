0a2b5c82c03991b483b17ab632015eb932e87a98
marvin <sea-son@foxmail.com>
2024-09-03 11:34:01 +0800

    feat: ollama

--- apps/core/date-tips-backend/src/controller/chat-controller/index.ts
+++ apps/core/date-tips-backend/src/controller/chat-controller/index.ts
@@ -1,8 +1,30 @@
 import {Router} from "express";
-
+import {streamText} from "ai";
+import {createOllama} from "ollama-ai-provider";
+const ollama = createOllama({
+  baseURL: 'http://10.3.74.72:11434',
+});
 const ChatController = Router();
 
 ChatController.get('/hello', (req, res) => res.send('Hello World!'))
 
+ChatController.get('/stream', async (req, res) => {
+  try {
+    const result = await streamText({
+      model: ollama('qwen2:7b'),
+      system: `You are a helpful, respectful and honest assistant.`,
+      messages: [
+        {
+          role: 'user',
+          content: 'Hello, how are you?'
+        }
+      ],
+    })
 
+
+    return res.json({result})
+  } catch (e) {
+    console.log(e)
+  }
+})
 export default ChatController
