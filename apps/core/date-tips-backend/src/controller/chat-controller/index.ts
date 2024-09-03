16b2cd0298af9164f00ca8fa0f40ae25ced17c70
marvin <sea-son@foxmail.com>
2024-09-03 16:32:10 +0800

    feat: ollama localhost 2

--- apps/core/date-tips-backend/src/controller/chat-controller/index.ts
+++ apps/core/date-tips-backend/src/controller/chat-controller/index.ts
@@ -1,6 +1,7 @@
 import {Router} from "express";
-import {streamText} from "ai";
+import {convertToCoreMessages, StreamData, streamText} from "ai";
 import {createOllama} from "ollama-ai-provider";
+
 const ollama = createOllama({
   baseURL: 'http://localhost:11434/api',
 });
@@ -10,28 +11,17 @@ ChatController.get('/hello', (req, res) => res.send('Hello World!'))
 
 ChatController.post('/stream', async (req, res) => {
   try {
-    const data = req.body;
-    console.log(data)
+    const {messages} = req.body;
+
+    console.log(messages)
     const result = await streamText({
       model: ollama('llama3.1:8b'),
-      system: `You are a helpful, respectful and honest assistant.`,
-      messages: [
-        {
-          role: 'user',
-          content: data.prompt
-        }
-      ],
+      messages: convertToCoreMessages(messages),
     })
-    const response = new Response();
-    if(response?.body){
-      // @ts-ignore
-      for await (const responseElement of response.body) {
-        console.log(responseElement)
-      }
-    }
 
-
-    return result.toTextStreamResponse()
+    return result.toTextStreamResponse({
+      status: 200
+    })
   } catch (e) {
     console.log(e)
   }
--- apps/core/date-tips/src/views/Chat/index.jsx
+++ apps/core/date-tips/src/views/Chat/index.jsx
@@ -2,31 +2,27 @@ import {useChat, useCompletion} from 'ai/react'
 
 console.log(useCompletion)
 export default function Chat() {
-    const {completion, input, handleInputChange, handleSubmit} = useCompletion({
+    // const {messages, input, handleInputChange, handleSubmit} = useChat({
+    //     api: '/api/chat/stream'
+    // })
+    const {messages, input, handleInputChange, handleSubmit} = useChat({
+        keepLastMessageOnError: true,
         api: '/api/chat/stream'
-    })
-    return <>
-        <div className="w-screen h-screen p-8 flex justify-center items-center flex-rows">
-            <div className="w-full h-full border rounded-lg relative">
-                <div className="p-8">
-                    <div className="bg-slate-500 text-white p-8 rounded-lg">
-                        {completion}
-                    </div>
+    });
+    console.log('messages', messages)
+    return (
+        <>
+            {messages.map(message => (
+                <div key={message.id}>
+                    {message.role === 'user' ? 'User: ' : 'AI: '}
+                    {message.content}
                 </div>
-                <div className="absolute bottom-0 w-full p-8">
+            ))}
 
-                    <form onSubmit={handleSubmit}>
-                        <div className="flex w-full items-center space-x-2">
-                            <input
-                                className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 flex-1"
-                                value={input} onChange={handleInputChange}/>
-                            <button type="submit" className={"w-4"}>
-                                <span className="sr-only">Send</span>
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </>
+            <form onSubmit={handleSubmit}>
+                <input name="prompt" value={input} onChange={handleInputChange}/>
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    );
 }
