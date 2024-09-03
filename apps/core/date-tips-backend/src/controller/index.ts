8ee2c31de787b16a63f40094c5702bd876f676cc
marvin <sea-son@foxmail.com>
2024-09-03 10:50:17 +0800

    feat: rename express-base to date-tips-backend

--- apps/core/date-tips-backend/src/controller/index.ts
+++ apps/core/date-tips-backend/src/controller/index.ts
@@ -1,10 +1,8 @@
 import {Router} from 'express';
-import AccountController from './account';
 import DateTipController from "./datetip-controller";
 
 const IndexController = Router();
 
-IndexController.use('/account', AccountController);
 IndexController.use('/datetip', DateTipController);
 
 export default IndexController
--- apps/core/express-based/src/controller/account/index.ts
+++
@@ -1,7 +0,0 @@
-import {Router} from 'express';
-
-const AccountController = Router();
-
-AccountController.get('/', (req, res) => res.send("hi"))
-
-export default AccountController
