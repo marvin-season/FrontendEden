f9b73837e29b2d4641438ae1cbaecbf52af4223b
marvin <sea-son@foxmail.com>
2024-09-04 22:17:00 +0800

    feat: update schema.prisma

--- apps/core/date-tips-backend/prisma/migrations/20240904141538_init/migration.sql
+++ apps/core/date-tips-backend/prisma/migrations/20240904141538_init/migration.sql
@@ -10,17 +10,20 @@ CREATE TABLE "DateTip" (
 -- CreateTable
 CREATE TABLE "ChatConversation" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
+    "name" TEXT,
     "conversationId" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "ChatMessage" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "conversationId" INTEGER NOT NULL,
+    "conversationId" TEXT NOT NULL,
     "content" TEXT NOT NULL,
-    CONSTRAINT "ChatMessage_conversationId_fkey" FOREIGN KEY ("conversationId") REFERENCES "ChatConversation" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "ChatMessage_conversationId_fkey" FOREIGN KEY ("conversationId") REFERENCES "ChatConversation" ("conversationId") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "ChatConversation_conversationId_key" ON "ChatConversation"("conversationId");
+
+-- CreateIndex
+CREATE INDEX "ChatConversation_fkey" ON "ChatMessage"("conversationId");
