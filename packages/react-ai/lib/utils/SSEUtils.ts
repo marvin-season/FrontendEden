8f60d5c8b55b8516ba3802ec8a6e221e5345e17f
marvin <sea-son@foxmail.com>
2024-09-04 16:29:35 +0800

    fix: 反序列化代码优化

--- packages/react-ai/lib/utils/SSEUtils.ts
+++ packages/react-ai/lib/utils/SSEUtils.ts
@@ -4,12 +4,16 @@ export const parseSSE = async (response: Response, onParse: (message: any) => vo
         return
     }
     // @ts-ignore
-    for await (const streamElement of response.body) {
-        const jsonV = textDecoder.decode(streamElement)
+    for await (const chunk of response.body) {
+        const sse_chunk = textDecoder.decode(chunk)
         try {
-            jsonV.split(/\n+/).forEach(line => {
-                const message = JSON.parse(line.replace(/data:\s*/, ''))
-                onParse(message)
+            sse_chunk.split(/\n+/).forEach(line => {
+                const json_obj = line.replace(/data:\s*/, '').trim() // '' or 'data: "{}"'
+                if (json_obj.length >= 4) {
+                    const message = JSON.parse(json_obj)
+                    onParse(message)
+                }
+
             })
 
         } catch (e) {
