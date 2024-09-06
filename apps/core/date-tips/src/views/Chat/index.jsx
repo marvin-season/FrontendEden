13c51bdb903fa56379e0c8b214fc24ed7e6e63e3
marvin <sea-son@foxmail.com>
2024-09-06 18:23:09 +0800

    fix: 切换会话中断

--- apps/core/date-tips/src/views/Chat/index.jsx
+++ apps/core/date-tips/src/views/Chat/index.jsx
@@ -1,79 +1,83 @@
-import {Chat, useChat, Types} from '@marvin/react-ai'
-import {useChatPage} from "./hooks/index.js";
-import React from "react";
+import { Chat, useChat, ChatActionType } from "@marvin/react-ai";
+import { useChatPage } from "./hooks/index.js";
+import React, { useEffect } from "react";
 
 export default function ChatPage() {
-    const {
-        conversations,
-        conversation,
-        fetchConversations,
-        selectConversation,
-        unSelectConversation,
-        deleteConversation
-    } = useChatPage()
+  const {
+    conversations,
+    conversation,
+    fetchConversations,
+    selectConversation,
+    unSelectConversation,
+    deleteConversation,
+  } = useChatPage();
 
-    const {reset, ...chatProps} = useChat({
-        async onSend(params, signal) {
-            console.log(params)
-            return await fetch('/api/chat/stream', {
-                method: 'POST',
-                signal,
-                body: JSON.stringify({...params, conversationId: conversation?.conversationId}),
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            });
-        }, onConversationEnd: async (lastMessage) => {
-            await fetchConversations()
-            await selectConversation(lastMessage.conversationId)
-            console.log('选中会话: ', lastMessage.conversationId)
-        }, onConversationStart: console.log, onStop: () => {
-        }
-    }, {
-        historyMessages: conversation?.messages || []
-    });
+  const { reset, ...chatProps } = useChat({
+    async onSend(params, signal) {
+      console.log(params);
+      return await fetch("/api/chat/stream", {
+        method: "POST",
+        signal,
+        body: JSON.stringify({ ...params, conversationId: conversation?.conversationId }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    },
+    onConversationEnd: async (lastMessage) => {
+      await fetchConversations();
+      await selectConversation(lastMessage.conversationId);
+      console.log("选中会话: ", lastMessage.conversationId);
+    },
+  }, {
+    historyMessages: conversation?.messages || [],
+  });
+
+  useEffect(() => {
+    conversation && chatProps.onAction(ChatActionType.StopGenerate);
+  }, [conversation]);
 
 
-    return <>
-        <div className={'flex gap-4'}>
-            <div className={'p-2'}>
-                <div onClick={() => {
-                    unSelectConversation();
-                }}>新建会话
-                </div>
-                {conversations.map(item => {
-                    return <div key={item.id} className={`flex gap-2 ${item.id === conversation?.id ? 'bg-blue-100' : ''} mb-2`}>
-
-                        <div onClick={() => selectConversation(item.conversationId)}>{item.name}</div>
-                        <div onClick={() => {
-                            confirm('确认删除吗?') && deleteConversation(item.conversationId).then(() => {
-                                unSelectConversation();
-                                fetchConversations();
-                                reset();
-                            })
-                        }}>删除
-                        </div>
-                    </div>
-                })}
-
-            </div>
-            <div className={'w-[800px] h-screen'}>
-                <Chat {...chatProps} title={'ChatBot'}/>
-            </div>
+  return <>
+    <div className={"flex gap-4"}>
+      <div className={"p-2"}>
+        <div onClick={() => {
+          unSelectConversation();
+        }}>新建会话
         </div>
-    </>;
+        {conversations.map(item => {
+          return <div key={item.id} className={`flex gap-2 ${item.id === conversation?.id ? "bg-blue-100" : ""} mb-2`}>
+
+            <div onClick={() => selectConversation(item.conversationId)}>{item.name}</div>
+            <div onClick={() => {
+              confirm("确认删除吗?") && deleteConversation(item.conversationId).then(() => {
+                unSelectConversation();
+                fetchConversations();
+                reset();
+              });
+            }}>删除
+            </div>
+          </div>;
+        })}
+
+      </div>
+      <div className={"w-[800px] h-screen"}>
+        <Chat {...chatProps} title={"ChatBot"} />
+      </div>
+    </div>
+  </>;
 }
 
-const AssistantMessageLayout = ({message, onRegenerate}) => {
-    return <>
-        <div className={'flex'}>
-            <div className={'bg-blue-300 text-white p-2'}>
-                {message.content}
-            </div>
-            <div className={'cursor-pointer'} onClick={() => {
-                onRegenerate?.(message);
-            }}>0️⃣
-            </div>
-        </div>
-    </>
-}
+const AssistantMessageLayout = ({ message, onRegenerate }) => {
+  return <>
+    <div className={"flex"}>
+      <div className={"bg-blue-300 text-white p-2"}>
+        {message.content}
+      </div>
+      <div className={"cursor-pointer"} onClick={() => {
+        onRegenerate?.(message);
+      }}>0️⃣
+      </div>
+    </div>
+  </>;
+};
--- packages/react-ai/lib/hooks/useChat.ts
+++ packages/react-ai/lib/hooks/useChat.ts
@@ -1,130 +1,135 @@
-import {nanoid} from "nanoid";
-import {useImmer} from "use-immer";
+import { nanoid } from "nanoid";
+import { useImmer } from "use-immer";
 import moment from "moment";
-import {ActionParams, ChatProps, Message} from "@/types";
+import { ActionParams, ChatProps, Message } from "@/types";
 import { useEffect, useMemo, useState } from "react";
-import {ChatActionType, ChatStatus} from "@/constant";
-import {SSEMessageGenerator} from "@/utils";
+import { ChatActionType, ChatStatus } from "@/constant";
+import { SSEMessageGenerator } from "@/utils";
 
-const format = 'YYYY-MM-DD HH:mm:ss';
+const format = "YYYY-MM-DD HH:mm:ss";
 
 type HandleProps = {
-    onSend: (params: ActionParams, signal: any) => Promise<Response>
-    onStop: Function,
-    onConversationEnd?: (message?: Message) => Promise<void>,
+  onSend: (params: ActionParams, signal: any) => Promise<Response>
+  onStop?: () => void,
+  onConversationEnd?: (message?: Message) => Promise<void>,
 }
 
 type ConfigProps = {
-    historyMessages: Message[]
+  historyMessages: Message[]
 }
 
 const ChatUtils: {
-    controller: AbortController | null
+  controller: AbortController | null
 } = {
-    controller: null
-}
+  controller: null,
+};
 
 export const useChat = (invokeHandle: HandleProps, config: ConfigProps = {
-    historyMessages: []
-}): ChatProps & {reset: Function} => {
-    const [messages, setMessages] = useImmer<Message[]>([]);
-    const [chatStatus, setChatStatus] = useState<ChatProps['status']>(ChatStatus.Idle);
-    /**
-     * @link: sendMessage
-     */
-    const mixedMessages = useMemo(() => {
-        if(chatStatus === ChatStatus.Idle) {
-            return config.historyMessages;
+  historyMessages: [],
+}): ChatProps & { reset: Function } => {
+  const [messages, setMessages] = useImmer<Message[]>([]);
+  const [chatStatus, setChatStatus] = useState<ChatProps["status"]>(ChatStatus.Idle);
+  /**
+   * @link: sendMessage
+   */
+  const mixedMessages = useMemo(() => {
+    if (chatStatus === ChatStatus.Idle) {
+      return config.historyMessages;
+    }
+    return [...config.historyMessages, ...messages];
+  }, [config.historyMessages, messages, chatStatus]);
+
+  // 发送消息任务(可能包含异步操作)
+  const executeSendTask = async (params: ActionParams) => {
+    ChatUtils.controller = new AbortController();
+    setChatStatus(ChatStatus.Loading);
+    setMessages(draft => {
+      draft.push({
+        id: nanoid(),
+        content: params.prompt as string,
+        createTime: moment().format(format),
+        role: "user",
+      });
+    });
+    return invokeHandle.onSend(params, ChatUtils.controller.signal);
+  };
+
+  // 接收消息任务(可能包含异步操作)
+  const executeReceiveTask = async (response: Response) => {
+    setChatStatus(ChatStatus.Typing);
+    let lastMessage;
+    for await (const message of SSEMessageGenerator<Message>(response)) {
+      lastMessage = message;
+      setMessages(draft => {
+        const find = draft.find(item => item.id === message.id);
+        if (find) {
+          find.content += message.content;
+        } else {
+          draft.push({ ...message, role: "assistant" });
         }
-        return [...config.historyMessages, ...messages]
-    }, [config.historyMessages, messages, chatStatus]);
-
-    // 发送消息任务(可能包含异步操作)
-    const executeSendTask = async (params: ActionParams) => {
-        ChatUtils.controller = new AbortController();
-        setChatStatus(ChatStatus.Loading);
-        setMessages(draft => {
-            draft.push({
-                id: nanoid(),
-                content: params.prompt as string,
-                createTime: moment().format(format),
-                role: 'user'
-            })
-        })
-        return invokeHandle.onSend(params, ChatUtils.controller.signal);
+      });
     }
 
-    // 接收消息任务(可能包含异步操作)
-    const executeReceiveTask = async (response: Response) => {
-        setChatStatus(ChatStatus.Typing);
-        let lastMessage;
-        for await (const message of SSEMessageGenerator<Message>(response)) {
-            lastMessage = message;
-            setMessages(draft => {
-                const find = draft.find(item => item.id === message.id);
-                if (find) {
-                    find.content += message.content
-                } else {
-                    draft.push({...message, role: 'assistant'})
-                }
-            })
-        }
+    invokeHandle.onConversationEnd?.(lastMessage);
+    setChatStatus(ChatStatus.Idle);
+  };
 
-        invokeHandle.onConversationEnd?.(lastMessage)
-        setChatStatus(ChatStatus.Idle);
+  /**
+   * 执行消息发送以及消息接受任务，任务完成后返回一次会话完成，此时拉取库中的数据
+   */
+  const sendMessage = async (params: ActionParams) => {
+    await executeReceiveTask(await executeSendTask(params));
+    return "一次会话完成";
+  };
+
+  const onSelectedFile = (files: FileList) => {
+    for (let index = 0; index < files.length; index++) {
+      const file = files[index];
+      if (file) {
+        const url = URL.createObjectURL(file);
+        console.log("🚀  ", url);
+      }
+
+    }
+  };
+
+  const stop = () => {
+    if (ChatStatus.Idle === chatStatus) {
+      return;
     }
 
-    /**
-     * 执行消息发送以及消息接受任务，任务完成后返回一次会话完成，此时拉取库中的数据
-     */
-    const sendMessage = async (params: ActionParams) => {
-        await executeReceiveTask(await executeSendTask(params));
-        return '一次会话完成'
-    }
 
-    const onSelectedFile = (files: FileList) => {
-        for (let index = 0; index < files.length; index++) {
-            const file = files[index]
-            if (file) {
-                const url = URL.createObjectURL(file);
-                console.log("🚀  ", url)
-            }
+    setChatStatus(ChatStatus.Idle);
+    ChatUtils.controller?.abort("stop");
+    invokeHandle.onStop?.();
+  };
 
-        }
-    }
+  const reset = () => {
+    stop();
+    setMessages([]);
+    setChatStatus(ChatStatus.Idle);
+  };
 
-    const stop = () => {
-        setChatStatus(ChatStatus.Idle);
-        ChatUtils.controller?.abort('stop')
-        invokeHandle.onStop();
-    }
+  useEffect(() => {
+    return () => {
+      reset();
+    };
+  }, []);
 
-    const reset = () => {
+  return {
+    reset,
+    messages: mixedMessages,
+    status: chatStatus,
+    onAction: (actionType, actionParams) => {
+      console.log("🚀  ", { actionType, actionParams });
+      if (actionType === ChatActionType.SendMessage || actionType === ChatActionType.ReloadMessage) {
+        sendMessage(actionParams as ActionParams).then();
+      } else if (actionType === ChatActionType.SelectAttachment) {
+        // onSelectedFile(actionParams.attachments);
+      } else if (actionType === ChatActionType.StopGenerate) {
         stop();
-        setMessages([]);
-        setChatStatus(ChatStatus.Idle);
-    }
+      }
+    },
+  };
 
-    useEffect(() => {
-        return () => {
-            reset()
-        }
-    }, []);
-
-    return {
-        reset,
-        messages: mixedMessages,
-        status: chatStatus,
-        onAction: (actionType, actionParams) => {
-            console.log("🚀  ", {actionType, actionParams});
-            if (actionType === ChatActionType.SendMessage || actionType === ChatActionType.ReloadMessage) {
-                sendMessage(actionParams as ActionParams).then()
-            } else if (actionType === ChatActionType.SelectAttachment) {
-                // onSelectedFile(actionParams.attachments);
-            } else if (actionType === ChatActionType.StopGenerate) {
-                stop();
-            }
-        }
-    }
-
-}
+};
