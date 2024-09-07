07be5492bc4834653f57fb7bbc2b688dc3f475c8
marvin <sea-son@foxmail.com>
2024-09-07 18:17:40 +0800

    fix: css

--- apps/core/date-tips/src/main.jsx
+++ apps/core/date-tips/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@marvin/tailwind-config/main.css';
+import './index.css'
 import App from "./App.jsx";
 
 createRoot(document.getElementById('root')).render(
