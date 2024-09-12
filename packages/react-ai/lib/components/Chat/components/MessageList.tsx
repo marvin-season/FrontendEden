00ff30f45d25f79f589e5febb87cfab7505395fe
marvin <sea-son@foxmail.com>
2024-09-12 11:23:45 +0800

    fix：chat 消息切换到最底部

--- packages/react-ai/lib/components/Chat/components/MessageList.tsx
+++ packages/react-ai/lib/components/Chat/components/MessageList.tsx
@@ -1,17 +1,34 @@
 import { Message } from "@/types";
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useRef } from "react";
 import { useChatContext } from "@/components/Chat/context/ChatContext.tsx";
 import { ChatActionType, ChatStatus } from "@/constant";
+
+const scrollToBottom = (element: HTMLDivElement) => {
+  if (element) {
+    console.log("scrollTo", element);
+    element.scrollTo({
+      top: element.scrollHeight,
+      behavior: "smooth",
+    });
+
+  }
+};
 
 export default function MessageList({ messages, status }: { messages: Message[], status?: ChatStatus }) {
   const { AssistantMessageLayout, UserMessageLayout, onAction } = useChatContext();
 
-  useEffect(() => {
+  const messagesContainerRef = useRef<HTMLDivElement>(null);
 
-  }, []);
+
+  useEffect(() => {
+    messagesContainerRef.current && scrollToBottom(messagesContainerRef.current);
+  }, [messages]);
   return (
     <>
-      <div className={'flex-grow overflow-y-auto border-slate-200 p-4 border rounded-lg'} id={"messages-container"}>
+      <div
+        id={"messages-container"}
+        ref={messagesContainerRef}
+        className={"flex-grow overflow-y-auto border-slate-200 p-4 border rounded-lg"}>
         {
           messages?.map((item, index) => {
             return (
