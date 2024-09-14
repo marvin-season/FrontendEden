6f72afa46b671e7e4c9eb14ef81db52f45889150
marvin <sea-son@foxmail.com>
2024-09-14 16:09:44 +0800

    fix：安全的反序列化

--- packages/react-ai/lib/utils/SSEUtils.ts
+++ packages/react-ai/lib/utils/SSEUtils.ts
@@ -8,18 +8,23 @@ export async function* SSEMessageGenerator<T>(response: Response) {
     // @ts-ignore
     for await (const chunk of response.body) {
         const sse_chunk = textDecoder.decode(chunk)
-        try {
-            // 使用for...of替代forEach，确保yield在生成器体内
-            for (const line of sse_chunk.split(/\n+/)) {
-                const json_obj = line.replace(/data:\s*/, '').trim();
-                if (json_obj.length >= 4) {
-                    const message = JSON.parse(json_obj);
+        let rest_str = ""
+        // 使用for...of替代forEach，确保yield在生成器体内
+        for (const line of sse_chunk.split(/\n+/)) {
+            const json_str = line.replace(/data:\s*/, '').trim();
+            if(json_str.length > 0) {
+                try {
+                    const message = JSON.parse(rest_str + json_str);
+                    rest_str = "";
                     yield message as T; // 在生成器内yield消息
+                } catch (e) {
+                    rest_str += json_str
+                    console.log("e => ", { e, rest_str });
                 }
             }
-        } catch (e) {
-            console.log(e)
+
         }
+
     }
 }
 
