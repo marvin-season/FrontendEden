0b189379804302ae062267de053c9c7c16ee4161
marvin <sea-son@foxmail.com>
2024-09-22 22:36:30 +0800

    fix: style

--- apps/core/date-tips/src/views/Chat/index.jsx
+++ apps/core/date-tips/src/views/Chat/index.jsx
@@ -20,25 +20,28 @@ export default function ChatPage() {
 
 
     return <>
-        <div className={"bg-white p-2 h-screen flex gap-4 justify-center"}>
-            <div className={"p-4 border rounded-xl bg-gray-100 b flex flex-col w-[300px]"}>
-                <ConversationBar
-                    checkoutConversation={chatProps.checkoutConversation}
-                    fetchConversations={fetchConversations}
-                    conversations={conversations}
-                    conversationId={chatProps.conversationId}
-                    setHistoryMessages={chatProps.setHistoryMessages}
-                    fetchConversationMessages={fetchConversationMessages}
-                    deleteConversation={deleteConversation}
-                />
-            </div>
-            <div className={"flex-[3] w-[50%] border rounded-xl"}>
-                <Chat {...chatProps}/>
-            </div>
-            <div className={"flex-1 border rounded-xl p-4 text-white bg-gray-400 flex flex-col"}>
-                <EvalPanel state={approachHandle.state}/>
+        <div className={"bg-gradient-to-r from-green-200  to-blue-200  h-screen flex items-center justify-center"}>
+            <div className={"bg-[#fffa] rounded-[16px] h-[80%] w-[80%] p-16 flex gap-4 justify-center"}>
+                <div className={"p-4 border rounded-xl bg-gray-100 b flex flex-col w-[300px]"}>
+                    <ConversationBar
+                        checkoutConversation={chatProps.checkoutConversation}
+                        fetchConversations={fetchConversations}
+                        conversations={conversations}
+                        conversationId={chatProps.conversationId}
+                        setHistoryMessages={chatProps.setHistoryMessages}
+                        fetchConversationMessages={fetchConversationMessages}
+                        deleteConversation={deleteConversation}
+                    />
+                </div>
+                <div className={"flex-[3] w-[50%] border rounded-xl"}>
+                    <Chat {...chatProps}/>
+                </div>
+                <div className={"flex-1 border rounded-xl p-4 text-white bg-gray-400 flex flex-col"}>
+                    <EvalPanel state={approachHandle.state}/>
+                </div>
             </div>
         </div>
+
     </>;
 }
 
