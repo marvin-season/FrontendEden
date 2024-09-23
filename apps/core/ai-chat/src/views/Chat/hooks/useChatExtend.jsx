454e98cb4c777eb986c278b11ec74ff879720932
marvin <sea-son@foxmail.com>
2024-09-23 20:46:16 +0800

    perf: 格式化

--- apps/core/ai-chat/src/views/Chat/hooks/useChatExtend.jsx
+++ apps/core/ai-chat/src/views/Chat/hooks/useChatExtend.jsx
@@ -1,84 +1,84 @@
-import {useChat} from "@marvin/react-ai";
+import { useChat } from "@marvin/react-ai";
 import React, { useState } from "react";
 
-const useChatExtend = ({approachHandle, fetchConversations, conversations = []}) => {
-    const [tools, setTools] = useState([
-      {id: 1, name: '天气', selected: false},
-      {id: 2, name: '翻译', selected: false}
-    ]);
+const useChatExtend = ({ approachHandle, fetchConversations, conversations = [] }) => {
+  const [tools, setTools] = useState([
+    { id: 1, name: "天气", selected: false },
+    { id: 2, name: "翻译", selected: false },
+  ]);
 
-    const [docs, setDocs] = useState([
-      {id: 1, name: '资本论', selected: false},
-      {id: 2, name: '肖生克的救赎', selected: false}
-    ]);
+  const [docs, setDocs] = useState([
+    { id: 1, name: "资本论", selected: false },
+    { id: 2, name: "肖生克的救赎", selected: false },
+  ]);
 
-    const chatProps = useChat({
-        async onSend(params, signal) {
-            return approachHandle.getApproach.call(null, params, signal);
-        },
-        onConversationStart: async (lastMessage) => {
-            // 如果是新对话
-            if (!conversations.find(item => item.conversationId === lastMessage.conversationId)) {
-                await fetchConversations();
-            }
-        },
-    }, {});
+  const chatProps = useChat({
+    async onSend(params, signal) {
+      return approachHandle.getApproach.call(null, params, signal);
+    },
+    onConversationStart: async (lastMessage) => {
+      // 如果是新对话
+      if (!conversations.find(item => item.conversationId === lastMessage.conversationId)) {
+        await fetchConversations();
+      }
+    },
+  }, {});
 
-    const AssistantMessageLayout = ({message, onRegenerate}) => {
-        return <>
-            <div className={"flex"}>
-                <div className={"bg-gray-200 py-2 px-4 flex rounded-lg"}>
-                    {message.content}
-                </div>
-                <div className={"cursor-pointer"} onClick={() => {
-                    onRegenerate?.(message);
-                }}>RE
-                </div>
-            </div>
-        </>;
-    };
-
-
-    const UserMessageLayout = ({message, onRegenerate}) => {
-        return <>
-            <div className={"flex"}>
-                <div className={"bg-gray-400 text-white py-2 px-4 rounded-lg mb-2 mt-4"}>{message.content}</div>
-            </div>
-        </>
-    }
-
-    const commandElementRender = (commandChar, { onClose }) => {
-      const state_ = commandChar === "@" ? tools : docs;
-      const setState_ = commandChar === "@" ? setTools : setDocs;
-      return <div className={"h-[100px] rounded p-4 backdrop-blur-sm bg-[#fff9]"}>
-        <div className={"text-blue-600 text-xl font-bold italic"}>{commandChar}</div>
-        <div className={"flex flex-wrap gap-2"}>
-          {
-            state_.map((item, index) => {
-              return <div
-                className={`cursor-pointer text-gray-600 py-2 px-4 rounded-xl border text-sm ${item.selected ? "border-green-500" : ""}`}
-                key={index}
-                onClick={() => {
-                  setState_(prev => prev.map(item_ => {
-                    if (item_.id === item.id) {
-                      item_.selected = !item.selected;
-                    }
-                    return item;
-                  }));
-                  onClose();
-                }}>{item.name}</div>;
-            })
-          }
+  const AssistantMessageLayout = ({ message, onRegenerate }) => {
+    return <>
+      <div className={"flex"}>
+        <div className={"bg-gray-200 py-2 px-4 flex rounded-lg"}>
+          {message.content}
         </div>
-      </div>;
-    };
-    return {
-        ...chatProps,
-        commandElementRender,
-        AssistantMessageLayout,
-        UserMessageLayout,
-        title: ""
-    }
-}
+        <div className={"cursor-pointer"} onClick={() => {
+          onRegenerate?.(message);
+        }}>RE
+        </div>
+      </div>
+    </>;
+  };
 
-export default useChatExtend
+
+  const UserMessageLayout = ({ message, onRegenerate }) => {
+    return <>
+      <div className={"flex"}>
+        <div className={"bg-gray-400 text-white py-2 px-4 rounded-lg mb-2 mt-4"}>{message.content}</div>
+      </div>
+    </>;
+  };
+
+  const commandElementRender = (commandChar, { onClose }) => {
+    const state_ = commandChar === "@" ? tools : docs;
+    const setState_ = commandChar === "@" ? setTools : setDocs;
+    return <div className={"h-[100px] rounded p-4 backdrop-blur-sm bg-[#fff9]"}>
+      <div className={"text-blue-600 text-xl font-bold italic"}>{commandChar}</div>
+      <div className={"flex flex-wrap gap-2"}>
+        {
+          state_.map((item, index) => {
+            return <div
+              className={`cursor-pointer text-gray-600 py-2 px-4 rounded-xl border text-sm ${item.selected ? "border-green-500" : ""}`}
+              key={index}
+              onClick={() => {
+                setState_(prev => prev.map(item_ => {
+                  if (item_.id === item.id) {
+                    item_.selected = !item.selected;
+                  }
+                  return item;
+                }));
+                onClose();
+              }}>{item.name}</div>;
+          })
+        }
+      </div>
+    </div>;
+  };
+  return {
+    ...chatProps,
+    commandElementRender,
+    AssistantMessageLayout,
+    UserMessageLayout,
+    title: "",
+  };
+};
+
+export default useChatExtend;
