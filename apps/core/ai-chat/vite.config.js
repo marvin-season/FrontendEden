d6a508333c2ec1b982a8978e16a6bcc449c09842
marvin <sea-son@foxmail.com>
2024-09-26 15:48:21 +0800

    feat: 添加依赖分析插件

--- apps/core/ai-chat/vite.config.js
+++ apps/core/ai-chat/vite.config.js
@@ -1,9 +1,22 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        rollupOptions: {
+            plugins: [
+                visualizer({
+                    open: true, // 直接在浏览器中打开分析报告
+                    filename: 'stats.html', // 输出文件的名称
+                    gzipSize: true, // 显示gzip后的大小
+                    brotliSize: true, // 显示brotli压缩后的大小
+                })
+            ]
+        }
+    },
     server: {
         port: 7777,
         proxy: {
