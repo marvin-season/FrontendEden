48cf050db20177ee1169fcf4e3f8375c1e378f91
marvin <sea-son@foxmail.com>
2024-10-14 13:37:39 +0800

    fix: error

--- apps/core/react-research/src/pages/react-research-page/ScrollPanel/index.tsx
+++ apps/core/react-research/src/pages/react-research-page/ScrollPanel/index.tsx
@@ -1,6 +1,6 @@
 import './index.css'
 import {useState} from "react";
-import {Button} from "@marvin/react-ai";
+import { Button } from "antd";
 
 const ScrollPanel = () => {
     const [count, setCount] = useState(0)
