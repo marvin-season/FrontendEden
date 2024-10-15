88e1884bc0d85b752f35cd5b61ad4782140de782
marvin <sea-son@foxmail.com>
2024-10-15 15:18:14 +0800

    fix: 代码 effect 重复调用

--- apps/core/react-research/src/pages/react-research-page/UpdatePage/index.tsx
+++ apps/core/react-research/src/pages/react-research-page/UpdatePage/index.tsx
@@ -4,27 +4,28 @@ const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const UpdatePage = ({ text }: { text: string }) => {
   const [content, setContent] = useState("");
-  const isInitialRender = useRef(true);
+  const isPreRender = useRef(true);
+
+  const cleanup = () => {
+    isPreRender.current = false;
+
+  };
 
   useEffect(() => {
-    if (!isInitialRender.current) return;
-    isInitialRender.current = false;
+    if (!isPreRender.current) {
+      (async () => {
+        for (const word of text.split(" ")) {
+          // 如果不 await 这个 Promise,下一次循环不会等待
+          await sleep(0).then(() => {
+            setContent(prevState => prevState + " " + word);
+          });
+        }
+      })();
+    }
 
-    console.log("UpdatePage", text);
-
-    (async () => {
-      for (const word of text.split(" ")) {
-        // 如果不 await 这个 Promise,下一次循环不会等待
-        await sleep(0).then(() => {
-          setContent(prevState => prevState + " " + word);
-        });
-      }
-    })();
-
-    return () => {
-      isInitialRender.current = true;
-    };
+    return cleanup;
   }, [text]);
+
 
   return <>
     <div className={"text-wrap"}>
