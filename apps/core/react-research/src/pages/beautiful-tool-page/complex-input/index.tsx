153797840eba81c2fc893d0e51f54ef7c7b2a12a
marvin <sea-son@foxmail.com>
2024-10-16 14:47:12 +0800

    feat: ComplexInput style input

--- apps/core/react-research/src/pages/beautiful-tool-page/complex-input/index.tsx
+++ apps/core/react-research/src/pages/beautiful-tool-page/complex-input/index.tsx
@@ -1,5 +1,31 @@
-export const ComplexInput = () => {
-  return <>
+import { ReactNode, useEffect, useState } from "react";
 
-  </>
-}
+export const ComplexInput = () => {
+  const [value, setValue] = useState<ReactNode>();
+
+  useEffect(() => {
+    setValue(<span>白发苍苍</span>);
+    setValue(prev => <>
+      {prev}
+      <span className={"bg-blue-300 p-1 rounded"}>
+        <span contentEditable>world</span>
+      </span>
+      <span>{"pretty"}</span>
+      <span className={"bg-blue-300 p-1 rounded"} contentEditable={false}>
+        <span contentEditable>world</span>
+      </span>
+    </>);
+    setValue(prev => <>{prev}<span className={""}>{" hi"}</span></>);
+
+    return () => {
+      setValue(undefined);
+    };
+  }, []);
+  return <>
+    <div className={"px-4 py-2 border"} contentEditable={true}>
+      {
+        value
+      }
+    </div>
+  </>;
+};
