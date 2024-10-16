e5ff1ba04e01acfb6ebbd58f20df84536b13781d
marvin <sea-son@foxmail.com>
2024-10-16 15:11:32 +0800

    feat: ComplexInput style input => 删除一个节点

--- apps/core/react-research/src/pages/beautiful-tool-page/complex-input/index.tsx
+++ apps/core/react-research/src/pages/beautiful-tool-page/complex-input/index.tsx
@@ -1,29 +1,43 @@
 import { ReactNode, useEffect, useState } from "react";
 
 export const ComplexInput = () => {
-  const [inputs, setInputs] = useState<{ type: "text" | "element", value: string }[]>([
+  const [inputs, setInputs] = useState<{ id: string; type: "text" | "element", value: string }[]>([
     {
+      id: "1",
       type: "text",
       value: "白发苍苍",
     },
     {
+      id: "2",
+
       type: "element",
       value: "hello",
     },
     {
+      id: "3",
       type: "text",
       value: "pretty",
     },
     {
+      id: "4",
       type: "element",
       value: "world",
     },
   ]);
 
+  console.log("inputs", inputs);
+
   return <>
-    <div className={"px-4 py-2 border"} contentEditable={true}>
+    <div className={"px-4 py-2 border"} contentEditable={true} onKeyDown={(e) => {
+      if (e.key !== "Backspace") {
+        return;
+      }
+      setInputs(prev => {
+        return prev.filter(item => item.id !== "2");
+      });
+    }}>
       {
-        inputs.map((input, index) => {
+        inputs?.map((input, index) => {
           return <>
             {
               input.type === "text" ? <span data-type={input.type} key={index}>{input.value}</span> :
