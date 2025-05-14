fa4dcbc6856d88141963e568d86e4a12fcbe2766
marvin <sea-son@foxmail.com>
2025-05-14 10:38:09 +0800

    👌 IMPROVE: agent tag

--- apps/core/react-research/src/pages/markdown-page/react-markdown/index.tsx
+++ apps/core/react-research/src/pages/markdown-page/react-markdown/index.tsx
@@ -1,8 +1,9 @@
 import Markdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
+import type { Components } from 'react-markdown'
 
 type Props = {}
-const MyDiv = ({ children, ...props }: React.HTMLAttributes<HTMLDivElement>) => {
+const Agent = ({ children, ...props }: React.HTMLAttributes<HTMLDivElement>) => {
     return (
         <div style={{ border: '1px solid red', padding: '8px' }} {...props}>
             👋 自定义组件内容：
@@ -14,16 +15,19 @@ const source = `
 # Hello, *world*!
 This is a simple markdown example using **react-markdown**.
 
-<div>hi</div>
+<agent>this is agent</agent>
 `
+
+const components: Components & {
+  agent?: React.ElementType
+} = {
+  agent: ({ node, ...props }) => <Agent {...props} />,
+}
 
 export default function ReactMarkdownTest({ }: Props) {
     return (
         <div>
-            <Markdown rehypePlugins={[rehypeRaw]}
-                components={{
-                    div: ({ node, ...props }) => <MyDiv {...props} />,
-                }}>
+            <Markdown rehypePlugins={[rehypeRaw]} components={components}>
                 {source}
             </Markdown>
         </div>
